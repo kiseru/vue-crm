cc6eb34be999f1353c192f35dfadefdd4f5bf1ec
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-18 20:02:50 +0300

    added initial styles

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@ export default {
 </script>
 
 <style>
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
